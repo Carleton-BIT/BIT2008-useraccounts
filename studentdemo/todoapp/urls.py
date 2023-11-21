c3357481a90715f4ab0f91cc1955be257c65729b
James Brunet <james@brunet.dev>
2023-11-21 13:03:00 -0500

    Add support for user accounts and per-user to-do lists

--- studentdemo/todoapp/urls.py
+++ studentdemo/todoapp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create_task/', views.create_task, name='create_task')
+    path('create_task/', views.create_task, name='create_task'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- studentdemo/todoapp/views.py
+++ studentdemo/todoapp/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from .models import Task
 from django.shortcuts import render, redirect
 from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def index(request):
-    # Fetch Data From Model
-    tasks = Task.objects.all()
+    # Fetch Data From Model for the current user
+    tasks = Task.objects.filter(user=request.user)
 
     # Fetch Form from Forms
     form = TaskForm()
@@ -13,8 +16,11 @@ def index(request):
     # Insert Model Data into Template, Run Template Code, an Return to Client
     return render(request, 'index.html', {'tasks': tasks, 'form': form})
 
+@login_required
 def create_task(request):
     form = TaskForm(request.POST)
     if form.is_valid():
-        form.save()
+        task = form.save(commit=False)
+        task.user = request.user
+        task.save()
         return redirect('index')  # Redirect to wherever you want after saving
